c960ec1706e3f9c92494bacb5cbe2fcb6f39d923
Nthenge <abrahamnetsec@gmail.com>
2025-02-10 13:10:17 +0300

    Added all about me section content, it needs styling

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,24 +1,28 @@
 import zigzag from "../Icons/zigzag.png"
-import favicon from "../Icons/favicon.png"
+import square from "../Icons/square.png"
+import { FaKey } from "react-icons/fa";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faFacebook,faXTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 export const About = () => {
     return (
         <div className="aboutMe">
             <section className="section-onee">
                 <header>
-                    <p>Abr<span><img src={favicon}/></span>ham</p>
+                    <p className="fakey"><FaKey/></p>                
                 </header>
-                <div>
+                <div className="abouts">
+                    <header>
+                        <p> 
+                            Desing
+                        </p>
+                    </header>
                     <div className ="dottts-grid">
                         {[...Array(144)].map((_, index)=>{
                         return <div key={index} className="dottt"></div>
                         })}
                     </div>
+                    
                     <div className="notesaboutme">
-                        <header>
-                            <p>
-                                Desing
-                            </p>
-                        </header>
                         <p>
                         I'm probably not the typical designer positioned behind an<br/>
                         Illustrator artboard adjusting pixels, but I design. Immersed in <br/>
@@ -27,9 +31,11 @@ export const About = () => {
                         experiences while staying fashionable.
                         </p>
                     </div>
-                    <p>
-
-                    </p>
+                    <div className ="dotttss-grid">
+                        {[...Array(5)].map((_, index)=>{
+                        return <div key={index} className="dotttt"></div>
+                        })}
+                    </div>
                     <div className="zigzag-image">
                         <img src={zigzag} alt="zigzag"/>   
                     </div>
@@ -37,10 +43,17 @@ export const About = () => {
             </section>
 
             <section className="section-twoo">
-                <div className ="dotts-grid">
-                    {[...Array(144)].map((_, index)=>{
-                    return <div key={index} className="dott"></div>
-                    })}
+                <div className="grid-dots">
+                    <div className ="dotttss-grid">
+                        {[...Array(5)].map((_, index)=>{
+                        return <div key={index} className="dotttt"></div>
+                        })}
+                    </div>
+                    <div className ="dotts-grid">
+                        {[...Array(144)].map((_, index)=>{
+                        return <div key={index} className="dott"></div>
+                        })}
+                    </div>
                 </div>
                 <div className="notesaboutme">
                     <header>
@@ -52,6 +65,13 @@ export const About = () => {
                         In building JavaScript applications, I'm equipped with just the <br/>right tools, and can absolutely function independently of them <br/>to deliver fast, resilient solutions optimized for scale — <br/>performance and scalabilty are priorities on my radar.
                     </p>
                 </div>
+
+                <div className="shapes">
+                    <span><img src={square} alt="square"/></span>
+                    <span className="Icon"><FontAwesomeIcon icon={faGithub} /></span>
+                    <span className="Icon"><FontAwesomeIcon icon={faFacebook}/></span>
+                    <span className="Icon"><FontAwesomeIcon icon={faXTwitter}/></span>
+                    <span className="Icon"><FontAwesomeIcon icon={faInstagram}/></span></div>
             </section>
             
         </div>
